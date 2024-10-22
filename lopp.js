a85320985f6279994a014bb5184b67958044c56f
Nadia zerin <nadiajerin10@gmail.com>
2024-10-22 22:51:54 +0600

    loop diye natural  number jog kora

---
+++ lopp.js
@@ -0,0 +1,21 @@
+// print numbers from 1 to 5
+/*let i;
+for (i = 1; i <= 5; i++) {
+  console.log("i=", i);
+}*/
+//while loop
+/*let count = 1;
+while (count <= 5) {
+  console.log("count=", count);
+  count++;
+}*/
+//do while loop
+/*let j = 1;
+do {
+  console.log("j=", j);
+  j++;
+} while (j <= 5);*/
+// display sum of n natural  number
+let n;
+n = parseInt(prompt("Enter a number"));
+console.log(n);
--- userinputt/prommpt.js
+++ userinputt/prommpt.js
@@ -13,3 +13,28 @@ while (gameNum != userNum) {
   userNum = parseInt(prompt("You entired wrong guess! pls try again"));
 }
 console.log("congratulations !!");
+//for loop
+let n;
+n = parseInt(prompt("Enter a number"));
+let sum = 0;
+for (let i = 1; i <= n; i++) {
+  console.log("sum is=", (sum = sum + i));
+}
+//while loop
+let m;
+let sum = 0;
+m = parseInt(prompt("Enter a number "));
+let j = 1;
+while (j <= m) {
+  console.log("sum is ", (sum = sum + j));
+  j++;
+}
+//do-while loop
+let r;
+r = parseInt(prompt("Enter a number "));
+let sum = 0;
+let k = 1;
+do {
+  console.log("sum is=", (sum = sum + k));
+  k++;
+} while (k <= r);
